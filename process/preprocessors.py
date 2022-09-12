3855d8cae20dc64dedfee58a880a2850542448a4
AmeerAli Khan <ameerali.khan@munit-452.local>
2022-09-12 12:40:27 +0200

    Analyze the techniqye

--- process/preprocessors.py
+++ process/preprocessors.py
@@ -16,7 +16,7 @@ def read_data():
     files = ["mongodb","react","socketio"]
     frames = []
     for file in files:
-        df = pd.read_csv(f"./data/{file}.csv",sep=";",encoding="utf-8")
+        df = pd.read_csv(f"../data/{file}.csv",sep=";",encoding="utf-8")
         df["source"] = [f"{file}" for _ in range(len(df))]
         frames.append(df)
     return pd.concat(frames)
